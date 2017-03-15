db02a04b7c1c19282d48f27c744d90ddf80db216
zhuguangxiang <zhuguangxiang@163.com>
2017-03-15 21:27:46 +0800

    delete files

--- koala_ast.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef _KOALA_AST_H_
-#define _KOALA_AST_H_
-
-BEGIN_DECLS /* 兼容C++编译宏 */
-
-END_DECLS /* 兼容C++编译宏 */
-
-#endif
--- koala_compiler.c
+++
@@ -1,7 +0,0 @@
-
-void koala_symbol_table_init();
-
-void koala_compiler_init()
-{
-  koala_symbol_table_init();
-}
